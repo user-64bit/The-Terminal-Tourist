a10fbcb16c022376d2727a3028be6d66477e561b
arth2002 <arthprajapati6602@gmail.com>
2024-02-27 21:18:04 +0530

    Link on title

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,7 +4,12 @@ export default function Navbar() {
 	return (
 		<>
 			<div className='border sticky top-0 z-50 bg-[#212529]'>
-				<h1 className='text-center text-2xl py-5'><span className="text-red-500">T</span>he<span className="text-red-500">T</span>erminal<span className="text-red-500">T</span>ourist</h1>
+				<Link href={"/"}>
+					<h1 className='text-center text-2xl py-5'>
+						<span className="text-red-500">T</span>he
+						<span className="text-red-500">T</span>erminal
+						<span className="text-red-500">T</span>ourist</h1>
+				</Link>
 			</div>
 		</>
 	)
