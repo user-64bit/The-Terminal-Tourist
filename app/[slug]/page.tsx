7fd8bfa3fb54891189fd92853648dbf0628b7df6
arth2002 <arthprajapati6602@gmail.com>
2024-02-29 12:11:17 +0530

    Tweak in mdx design

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,32 +3,39 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import matter from "gray-matter";
 import "./markdown.css";
 import rehypePrettyCode from "rehype-pretty-code";
-import overnight from "overnight/themes/Overnight-Slumber.json";
 import path from "path";
 import Loader from "@/components/Loader";
 import { Suspense } from "react";
 
 export default async function BlogPage({ params }: { params: { slug: string } }) {
-    if(params.slug==="favicon.ico"){
+    if (params.slug === "favicon.ico") {
         return;
     }
     const abs_path = path.resolve("content")
     const filename = `${abs_path}/${params.slug}/index.md`;
-    
+
     const file = await fs.readFile(filename, "utf-8")
     const { content, data } = matter(file);
-
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let date = new Date(data.date);
     return (
         <Suspense fallback={<Loader />}>
             <article className="ms-14 me-10">
-                <h1 className="text-center py-4 text-3xl underline text-red-500">{data.title}</h1>
+                <h1 className="pt-4 pb-2 text-3xl text-white font-bold">{data.title}</h1>
+                <p className="opacity-45">
+                    <span>{data.auther}/</span>
+                    <span>{months[date.getMonth()] + " " + date.getDate() + "," + date.getFullYear()}</span>
+                </p>
+                <hr className="pb-10" />
                 <div className="">
                     <div className="markdown">
                         <MDXRemote source={content} options={{
                             mdxOptions: {
                                 remarkPlugins: [],
                                 // @ts-ignore
-                                rehypePlugins: [[rehypePrettyCode, { theme: overnight }]],
+                                rehypePlugins: [[rehypePrettyCode, {
+                                    theme: 'one-dark-pro'
+                                }]],
                             }
                         }} />
                     </div>
